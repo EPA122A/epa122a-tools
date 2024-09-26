0933109f1736f071e8ca4c86501e2bf5ff165bd7
Giacomo Marangoni <3717222+jackjackk@users.noreply.github.com>
2024-09-26 21:30:58 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='epa122a_tools',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    description='A simple data downloader package for EPA122A',
+    author='Giacomo Marangoni',
+    author_email='G.Marangoni@tudelft.nl',
+    url='https://github.com/EPA122A/epa122a-tools',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
